7f9770718fe713ccb6b857351b0abf12fdaad44d
Harsha <harxshz2004@gmail.com>
2026-06-29 22:04:08 +0530

    Day 1: Initial Java Lab setup

---
+++ Variable.java
@@ -0,0 +1,12 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name = "harsha";
+        int age = 20;
+        double hight = 5.5;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(hight);
+    }
+
+}
