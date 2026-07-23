94a7eac407fe5c7bad3c2a98289f5b4726ad3c4b
Harsha <harxshz2004@gmail.com>
2026-07-23 21:44:30 +0530

    Add Java program demonstrating type casting

---
+++ Type_Cast.java
@@ -0,0 +1,39 @@
+public class Type_Cast {
+
+    public static void main(String[] args) {
+
+        int num = 100;
+        double d = num;
+
+        System.out.println("Implicit Type Casting (Widening)");
+        System.out.println("Integer value : " + num);
+        System.out.println("Double value  : " + d);
+
+        System.out.println();
+
+        double pi = 3.14159;
+        int value = (int) pi;
+
+        System.out.println("Explicit Type Casting (Narrowing)");
+        System.out.println("Double value  : " + pi);
+        System.out.println("Integer value : " + value);
+
+        System.out.println();
+
+        char ch = 'A';
+        int ascii = ch;
+
+        System.out.println("Character to Integer");
+        System.out.println("Character : " + ch);
+        System.out.println("ASCII Value : " + ascii);
+
+        System.out.println();
+
+        int code = 66;
+        char letter = (char) code;
+
+        System.out.println("Integer to Character");
+        System.out.println("Integer : " + code);
+        System.out.println("Character : " + letter);
+    }
+}
