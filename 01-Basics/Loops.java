2a746433fef435b6f53c2add1d8af7b5348d7936
Harsha <harxshz2004@gmail.com>
2026-08-07 20:02:29 +0530

    loop demonstrations with additional output statements in Loops.java

--- 01-Basics/Loops.java
+++ 01-Basics/Loops.java
@@ -1,13 +1,21 @@
 public class Loops {
     public static void main(String[] args) {
+        System.out.println("for-loop demonstration");
         for (int i = 0; i < 5; i++) {
             System.out.println(i);
         }
+        System.out.println("while-loop demonstration");
         int j = 1;
         while (j <= 5) {
             System.out.println(j);
             j++;
         }
+        System.out.println("do-while-loop demonstration");
+        int k = 1;
+        do {
+            System.out.println(k);
+            k++;
+        } while (k <= 5);
     }
 
 }
