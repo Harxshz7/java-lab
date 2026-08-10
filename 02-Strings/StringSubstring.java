e443a1118c125dd7f77dec52d79e938210e16274
Harsha <harxshz2004@gmail.com>
2026-08-10 14:31:28 +0530

    Add StringSubstring class for demonstrating substring extraction

---
+++ 02-Strings/StringSubstring.java
@@ -0,0 +1,8 @@
+public class StringSubstring {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        String substr = str.substring(7, 12);
+        System.out.println("Substring: " + substr);
+    }
+
+}
