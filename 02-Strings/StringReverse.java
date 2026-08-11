019919bc8b1d2553364733775afa9a19672af2c4
Harsha <harxshz2004@gmail.com>
2026-08-11 12:55:11 +0530

    Add StringReverse class for demonstrating string reversal in a string

--- 02-Strings/StringReverse.java
+++ 02-Strings/StringReverse.java
@@ -1,3 +1,16 @@
 public class StringReverse {
+    public static void main(String[] args) {
+        String str = "method";
+        char[] ch = str.toCharArray();
+        int i = 0, j = ch.length - 1;
+        while (i < j) {
+            char temp = ch[i];
+            ch[i] = ch[j];
+            ch[j] = temp;
+            i++;
+            j--;
+        }
+        System.out.println("reversed String is " + new String(ch));
+    }
 
 }
