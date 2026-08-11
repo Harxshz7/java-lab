8f957d2784cc3e6eb869700212ad28a9ecb086da
Harsha <harxshz2004@gmail.com>
2026-08-11 12:47:10 +0530

    Add StringReplace class for demonstrating string replacement in a string

--- 02-Strings/StringReplace.java
+++ 02-Strings/StringReplace.java
@@ -1,3 +1,9 @@
 public class StringReplace {
+    public static void main(String[] args) {
+        String str = "welcome to python programming";
+        String newStr = str.replace("python", "java");
+        System.out.println("Original String: " + str);
+        System.out.println("New String: " + newStr);
+    }
 
 }
