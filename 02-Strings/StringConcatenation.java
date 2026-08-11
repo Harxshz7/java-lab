8f747c3e11fa81f86b61e26c3ffe5d68071ec507
Harsha <harxshz2004@gmail.com>
2026-08-11 12:38:53 +0530

    Add StringConcatenation class for demonstrating string concatenation

--- 02-Strings/StringConcatenation.java
+++ 02-Strings/StringConcatenation.java
@@ -1,3 +1,8 @@
 public class StringConcatenation {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2x = "world";
+        System.out.println("concatenated string: " + str1 + " " + str2x);
+    }
 
 }
