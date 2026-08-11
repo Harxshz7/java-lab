96cd6fa6879811f9186a391b40a9fe443b8e34bf
Harsha <harxshz2004@gmail.com>
2026-08-11 12:35:00 +0530

    Add CountVowels class for counting and displaying vowels in a string

--- 02-Strings/Practice/CountVowels.java
+++ 02-Strings/Practice/CountVowels.java
@@ -2,4 +2,16 @@ package Practice;
 
 public class CountVowels {
 
+    public static void main(String[] args) {
+
+        String str = "javaprogramming";
+
+        String vowels = str.replaceAll("[^aeiouAEIOU]", "");
+
+        final int count = vowels.length();
+
+        System.out.println("Original String: " + str);
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Number of vowels: " + count);
+    }
 }
