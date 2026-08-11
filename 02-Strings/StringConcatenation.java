3987c9c3a43200cbb4e6629ce8e8b3eb3be72e92
Harsha <harxshz2004@gmail.com>
2026-08-11 12:44:59 +0530

    Add StringPalindrome class for checking if a string is a palindrome

--- 02-Strings/StringConcatenation.java
+++ 02-Strings/StringConcatenation.java
@@ -1,8 +1,8 @@
 public class StringConcatenation {
     public static void main(String[] args) {
         String str1 = "Hello";
-        String str2x = "world";
-        System.out.println("concatenated string: " + str1 + " " + str2x);
+        String str2 = "world";
+        System.out.println("concatenated string: " + str1 + " " + str2);
     }
 
 }
--- 02-Strings/StringPalindrome.java
+++ 02-Strings/StringPalindrome.java
@@ -1,3 +1,21 @@
 public class StringPalindrome {
+    public static void main(String[] args) {
+        String str = "level";
+        char[] charArray = str.toCharArray();
+        int i = 0, j = charArray.length - 1;
+        while (i < j) {
+            char temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            i++;
+            j--;
+        }
+        String reversed = new String(charArray);
+        if (str.equals(reversed)) {
+            System.out.println(str + " is a palindrome");
+        } else {
+            System.out.println(str + " is not a palindrome");
+        }
+    }
 
 }
