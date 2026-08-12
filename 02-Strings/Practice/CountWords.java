2699579a46d8a9a27f8770a6c53befe6b3962a5f
Harsha <harxshz2004@gmail.com>
2026-08-12 18:42:05 +0530

    Add CountWords class for counting and displaying words in a string

--- 02-Strings/Practice/CountWords.java
+++ 02-Strings/Practice/CountWords.java
@@ -1,5 +1,16 @@
 package Practice;
 
 public class CountWords {
+    public static void main(String[] args) {
+        String str = "count the number of words in this string";
+        String[] words = str.split(" ");
+        int count = 0;
+        for (int i = 0; i < words.length; i++) {
+            System.out.println(words[i]);
+            count++;
+        }
+        System.out.println("Original String: " + str);
+        System.out.println("Number of words: " + count);
+    }
 
 }
