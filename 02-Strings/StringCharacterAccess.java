2c1e1656023966398422012b18eb95e43854c090
Harsha <harxshz2004@gmail.com>
2026-08-12 18:45:57 +0530

    Add StringCharacterAccess class for accessing first and last characters of a string

--- 02-Strings/StringCharacterAccess.java
+++ 02-Strings/StringCharacterAccess.java
@@ -1,3 +1,12 @@
 public class StringCharacterAccess {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        char firstChar = str.charAt(0);
+        char lastChar = str.charAt(str.length() - 1);
+
+        System.out.println("Original String: " + str);
+        System.out.println("First character: " + firstChar);
+        System.out.println("Last character: " + lastChar);
+    }
 
 }
