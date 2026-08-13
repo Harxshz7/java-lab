26f22d45422fc8ed87bcd442e9ddf91ca585fc5c
Harsha <harxshz2004@gmail.com>
2026-08-13 19:38:00 +0530

    Add StringSplit class for splitting a string into words

--- 02-Strings/StringSplit.java
+++ 02-Strings/StringSplit.java
@@ -1,3 +1,12 @@
 public class StringSplit {
 
+    public static void main(String[] args) {
+        String str = "Hello, welecome to the world of Java programming!";
+        String[] words = str.split(" ");
+        System.out.println("Original String: " + str);
+        System.out.println("Split words:");
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
 }
