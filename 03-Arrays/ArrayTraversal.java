89269438be5ec2e7ad16201815630f829acd2c87
Harsha <harxshz2004@gmail.com>
2026-08-14 12:20:03 +0530

    ArrayTraversal class for traversing and displaying elements of an array

---
+++ 03-Arrays/ArrayTraversal.java
@@ -0,0 +1,10 @@
+public class ArrayTraversal {
+    public static void main(String[] args) {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        System.out.println("Taken array:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+
+}
