cbde4eb5ed17e036e7d8f3f1146e320e42b85a85
Harsha <harxshz2004@gmail.com>
2026-08-14 12:45:20 +0530

    Update string anagram program

--- 02-Strings/StringAnagram.java
+++ 02-Strings/StringAnagram.java
@@ -1,3 +1,34 @@
 public class StringAnagram {
+    public static void main(String[] args) {
 
+        String str = "abbc";
+        String str1 = "abcb";
+
+        int[] arr = new int[127];
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            arr[ch]++;
+        }
+
+        for (int i = 0; i < str1.length(); i++) {
+            char ch = str1.charAt(i);
+            arr[ch]--;
+        }
+
+        boolean isAnagram = true;
+
+        for (int i = 0; i < 127; i++) {
+            if (arr[i] != 0) {
+                isAnagram = false;
+                break;
+            }
+        }
+
+        if (isAnagram) {
+            System.out.println("The strings are anagrams.");
+        } else {
+            System.out.println("The strings are not anagrams.");
+        }
+    }
 }
