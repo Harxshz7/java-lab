81c01027cb167b751a7b680d17fc18fd4c595987
Harsha <harxshz2004@gmail.com>
2026-08-15 22:08:52 +0530

    Add String to char array example

--- 02-Strings/StringToCharArray.java
+++ 02-Strings/StringToCharArray.java
@@ -1,3 +1,12 @@
 public class StringToCharArray {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        char[] charArray = str.toCharArray();
+
+        System.out.println("Character array:");
+        for (char c : charArray) {
+            System.out.print(c + " ");
+        }
+    }
 
 }
