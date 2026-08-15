ee59406edf95f62f88c669a2b90a1f3ae95779cd
Harsha <harxshz2004@gmail.com>
2026-08-15 22:11:02 +0530

    Add String traversal example

--- 02-Strings/StringTraversal.java
+++ 02-Strings/StringTraversal.java
@@ -1,3 +1,9 @@
 public class StringTraversal {
-
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        System.out.println("Traversing the string:");
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i) + " ");
+        }
+    }
 }
