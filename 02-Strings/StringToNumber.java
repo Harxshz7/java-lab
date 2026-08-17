fb7c075960d96382f9c021b694c7b8a8a4e3a051
Harsha <harxshz2004@gmail.com>
2026-08-17 12:05:35 +0530

    Add String to number conversion example

--- 02-Strings/StringToNumber.java
+++ 02-Strings/StringToNumber.java
@@ -1,3 +1,11 @@
 public class StringToNumber {
+    public static void main(String[] args) {
 
+        String s1 = "141";
+
+        int a1 = Integer.parseInt(s1);
+
+        System.out.println("Original String: " + s1);
+        System.out.println("Converted Number: " + a1);
+    }
 }
