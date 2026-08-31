2737dfc02a4fa80b8342a0ad07f6cd46753be5d6
Harsha <harxshz2004@gmail.com>
2026-08-31 14:36:06 +0530

    example for method calling

---
+++ 04-Methods/MethodCalling.java
@@ -0,0 +1,11 @@
+
+public class MethodCalling {
+
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("this is example for method calling: " + addNumbers(10, 20));
+    }
+}
