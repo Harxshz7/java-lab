c0054be8ef04183ef564ad1dfc151037a09ffdfd
Harsha <harxshz2004@gmail.com>
2026-08-31 14:16:03 +0530

    Add static method example

---
+++ 04-Methods/StaticMethod.java
@@ -0,0 +1,11 @@
+public class StaticMethod {
+    static void displayMessage() {
+        System.out.println("Hello from the static method!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is a static method example.");
+        displayMessage();
+    }
+
+}
