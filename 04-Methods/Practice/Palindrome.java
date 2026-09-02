9dd115ab8f2424211b71c7174fe4c0e31b6fcbf4
Harsha <harxshz2004@gmail.com>
2026-09-02 18:40:41 +0530

    add Palindrome class to check if a string is a palindrome

---
+++ 04-Methods/Practice/Palindrome.java
@@ -0,0 +1,23 @@
+package Practice;
+
+public class Palindrome {
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String testString1 = "racecar";
+        System.out.println(testString1 + " is a palindrome: " + isPalindrome(testString1));
+
+    }
+}
