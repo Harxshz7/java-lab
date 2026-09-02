428f3e02a73a5630da7a9691be907b5bbfc86dfe
Harsha <harxshz2004@gmail.com>
2026-09-02 18:22:54 +0530

    adding two numbers using method

---
+++ 04-Methods/Practice/AddNumbers.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class AddNumbers {
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("this is example for method calling: " + addNumbers(10, 20));
+    }
+
+}
