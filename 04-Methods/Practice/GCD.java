26d6b6dd263f46f90277aa511b9ff0f08e8d502e
Harsha <harxshz2004@gmail.com>
2026-09-11 06:41:18 +0530

    add GCD class to calculate the greatest common divisor of two numbers

---
+++ 04-Methods/Practice/GCD.java
@@ -0,0 +1,19 @@
+package Practice;
+
+public class GCD {
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 48;
+        int num2 = 18;
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + gcd(num1, num2));
+    }
+}
